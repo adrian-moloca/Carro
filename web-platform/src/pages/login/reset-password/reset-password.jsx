7abb09b2d9769ebe59b1b1325981d6565183798d
AdrianUrs00 <adrian.urs@smartboxdigital.com>
2021-09-08 02:55:33 +0300

    my-packages-changebranch

--- web-platform/src/pages/login/reset-password/reset-password.jsx
+++ web-platform/src/pages/login/reset-password/reset-password.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {Container, Box} from '@material-ui/core';
 import PrimaryButton from '../../../components/buttons/primaryButton/primaryButton';
 import CarroTextField from '../../../components/textField/CarroTextField';
@@ -9,9 +10,11 @@ const ResetPassword = () =>{
             <Box mt={5} display='flex' justifyContent='center'><CarroTextField label='Parola noua' variant='outlined'/></Box>
             <Box mt={3}display='flex' justifyContent='center'><CarroTextField label='Confirmare parola' variant='outlined'/></Box>
             <Box mt = {10} display='flex' justifyContent='center'>
-                 <PrimaryButton variant='contained'>
-                        SALVEAZA
-                 </PrimaryButton>
+                <Link to='/login/' style={{ textDecoration: 'none' }}>
+                    <PrimaryButton variant='contained'>
+                            SALVEAZA
+                    </PrimaryButton>
+                </Link>
              </Box>
         </Container>
     );
--- web-platform/src/pages/my-packages/my-packages.jsx
+++ web-platform/src/pages/my-packages/my-packages.jsx
@@ -1,14 +1,146 @@
-import React from 'react'
-import './my-packages.css'
+import React from 'react';
+import useStyles from './my-packagesStyle';
+import { Container, Box, Typography, AccordionSummary, AccordionDetails, Accordion, Grid, } from '@material-ui/core';
+import {ExpandMore, ArrowForward, Delete, Create} from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core';
+const MyPackages = () => {
+  
+  const classes = useStyles();
 
-const MyPackages = () => { 
   return (
-    <div>
-      <p>
-      my-packages
-      </p>
-    </div>
-  )
-}
+    <Container className='Primary-container-style'>
+      <Box mb={2} fontWeight={400} fontSize={21} textAlign={'center'}>Pachetele mele</Box>
+      <Box mb={1.5} borderRadius='10px' boxShadow={10} >
+          <Accordion square='true' className={classes.AccordionBorderRadius}>
+            <AccordionSummary aria-controls="transport-content" id="transport-header">
+              <Grid container>
+                <Grid container item xs = {1} justifyContent='flex-starts'>
+                  <Typography >1.</Typography>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic' >Timisoara, Romania</Box>
+                </Grid>
+                <Grid container  item xs={1} justifyContent='center'>
+                    <ArrowForward className={'Primary-color'}/>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic'>Bucuresti, Romania</Box>
+                </Grid>
+                <Grid container item xs={3} justifyContent='center'>
+                  <Box >26/08/2021 02:00 AM</Box>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontSize={16} className={'Green-carro'}>Active</Box>
+                </Grid>
+                <Grid container item xs={1} justifyContent='flex-end'>
+                  <Create className={'Primary-color'}/>
+                  <Delete className={'Pink-carro'}/>
+                  <ExpandMore className={'Primary-color'}/>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails className={classes.AccordionDetailsFlex}>
+            </AccordionDetails>
+          </Accordion>
+      </Box>
+      <Box mb={1.5} borderRadius='10px' boxShadow={10} >
+          <Accordion square='true' className={classes.AccordionBorderRadius}>
+            <AccordionSummary aria-controls="transport-content" id="transport-header">
+                <Grid container>
+                <Grid container item xs = {1} justifyContent='flex-starts'>
+                  <Typography >1.</Typography>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic' >Timisoara, Romania</Box>
+                </Grid>
+                <Grid container  item xs={1} justifyContent='center'>
+                    <ArrowForward className={'Primary-color'}/>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic'>Bucuresti, Romania</Box>
+                </Grid>
+                <Grid container item xs={3} justifyContent='center'>
+                  <Box >26/08/2021 02:00 AM</Box>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontSize={16} className={'Green-carro'}>In Tranzit</Box>
+                </Grid>
+                <Grid container item xs={1} justifyContent='flex-end'>
+                  <Delete className={'Pink-carro'}/>
+                  <ExpandMore className={'Primary-color'}/>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails className={classes.AccordionDetailsFlex}>
+            </AccordionDetails>
+          </Accordion>
+      </Box>
+      <Box mb={1.5} borderRadius='10px' boxShadow={10} >
+          <Accordion square='true' className={classes.AccordionBorderRadius}>
+            <AccordionSummary aria-controls="transport-content" id="transport-header">
+            <Grid container>
+                <Grid container item xs = {1} justifyContent='flex-starts'>
+                  <Typography >1.</Typography>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic' >Timisoara, Romania</Box>
+                </Grid>
+                <Grid container  item xs={1} justifyContent='center'>
+                    <ArrowForward className={'Primary-color'}/>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic'>Bucuresti, Romania</Box>
+                </Grid>
+                <Grid container item xs={3} justifyContent='center'>
+                  <Box >26/08/2021 02:00 AM</Box>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontSize={16} className={'Primary-color'}>Finalizata</Box>
+                </Grid>
+                <Grid container item xs={1} justifyContent='flex-end'>
+                  <Delete className={'Pink-carro'}/>
+                  <ExpandMore className={'Primary-color'}/>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails className={classes.AccordionDetailsFlex}>
+            </AccordionDetails>
+          </Accordion>
+      </Box>
+      <Box mb={1.5} borderRadius='10px' boxShadow={10} >
+          <Accordion square='true' className={classes.AccordionBorderRadius}>
+            <AccordionSummary aria-controls="transport-content" id="transport-header">
+            <Grid container>
+                <Grid container item xs = {1} justifyContent='flex-starts'>
+                  <Typography >1.</Typography>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic' >Timisoara, Romania</Box>
+                </Grid>
+                <Grid container  item xs={1} justifyContent='center'>
+                    <ArrowForward className={'Primary-color'}/>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontWeight= {600} fontStyle='italic'>Bucuresti, Romania</Box>
+                </Grid>
+                <Grid container item xs={3} justifyContent='center'>
+                  <Box >26/08/2021 02:00 AM</Box>
+                </Grid>
+                <Grid container item xs={2} justifyContent='center'>
+                  <Box fontSize={16} className={'Pink-carro'}>Anulata</Box>
+                </Grid>
+                <Grid container item xs={1} justifyContent='flex-end'>
+                  <Delete className={'Pink-carro'}/>
+                  <ExpandMore className={'Primary-color'}/>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails className={classes.AccordionDetailsFlex}>
+            </AccordionDetails>
+          </Accordion>
+      </Box>
+    </Container>
+  );
+  };
 
 export default MyPackages;
