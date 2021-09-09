e30646ce5fc4f4fe114d73155a269da078f9fdff
AlexandruSlavoaca <alexandru.slavoaca@smartboxdigital.com>
2021-09-09 14:25:42 +0300

    add-package steps
    design implementation in progress

--- web-platform/src/pages/add-package/add-package.jsx
+++ web-platform/src/pages/add-package/add-package.jsx
@@ -10,7 +10,12 @@ import useStyles from './add-packageStyle.jsx'
 import { classes } from 'istanbul-lib-coverage';
 import CarroTextField from '../../components/textField/CarroTextField'
 import FormGroup from "@material-ui/core/FormGroup";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import AddIcon from '@material-ui/icons/Add';
 
 
 function getSteps() {
@@ -19,6 +24,7 @@ function getSteps() {
 
 const GetStepContent = (stepIndex) => {
 
+  // STEP 3 CHECKS
   const [stateCheckedInflamabil, setStateCheckedInflamabil] = React.useState({
     checkedA: false,
   });
@@ -45,6 +51,20 @@ const GetStepContent = (stepIndex) => {
     setStateCheckedAnimal({ ...stateCheckedAnimal, [event.target.name]: event.target.checked });
   };
    
+  // STEP 4 CHECK
+  const [stateSave, setStateSave] = React.useState({
+    checkedA: false,
+  });
+  const handleChangeSave = (event) => {
+    setStateSave({ ...stateSave, [event.target.name]: event.target.checked });
+  };
+
+  // STEP 4 RADIO BTNS
+    const [stateRadioBtns, setStateRadioBtns] = React.useState("female");
+    const handleChangeRadioBtns = (event) => {
+      setStateRadioBtns(event.target.value);
+    };
+
   switch (stepIndex) {
     case 0:
       return (
@@ -199,6 +219,94 @@ const GetStepContent = (stepIndex) => {
               direction="row"
               justifyContent="space-around"
               alignItems="center" >
+              <Grid item xs={5}>
+                <CarroTextField
+                  select
+                  label="Marimea pachetului"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <CarroTextField
+                  label="Greutatea pachetului"
+                  placeholder="KG" 
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Box>
+            <Grid 
+                container
+                direction="row"
+                justifyContent="center"
+                >
+              <CarroTextField
+                  fullWidth = {true}
+                  label="Scurta descriere"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+            </Grid>
+          </Box>
+          <Box my={4}>
+            <Grid 
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center" >
+              <Grid item xs={5}>
+                <CarroTextField
+                  select
+                  label="Pret"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <CarroTextField
+                  label="Numar pachete"
+                  placeholder="" 
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Box>
+            <Grid 
+                container
+                direction="row"
+                justifyContent="center"
+                >
+              <CarroTextField
+                  fullWidth = {true}
+                  label="Descriere"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+            </Grid>
+          </Box>
+          <Box my={4}>
+            <Grid 
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center" >
                 <Grid item xs={3}>
                   <FormGroup row>
                     <FormControlLabel
@@ -261,12 +369,112 @@ const GetStepContent = (stepIndex) => {
                 </Grid>
             </Grid>
           </Box>
+          <Box display='flex' direction='row' justifyContent='flex-end'>
+            <Button >
+              Adauga pachet
+              <AddIcon/>
+            </Button>
+          </Box>
         </Container>
       );
     case 3:
       return (
-        <Container className={"Primary-container-style"}>
-
+        <Container className="addPackagesContainer">
+          <Box mb={5} display='flex' direction='row' justifyContent='center'>
+            Modalitati de plata
+          </Box>
+          <Box mb={5} display='flex' direction='row' justifyContent='center'>
+            <FormControl component="fieldset">
+              <RadioGroup
+                value={stateRadioBtns}
+                onChange={handleChangeRadioBtns}
+              >
+                <Box display='flex' direction='row' justifyContent='center'>
+                  <FormControlLabel value="Card online" control={<Radio />} label="Card online" />
+                  <FormControlLabel value="Ordin de plata" control={<Radio />} label="Ordin de plata" />
+                  <FormControlLabel value="Ramburs" control={<Radio />} label="Ramburs" />
+                </Box>
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          <Box my={4}>
+            <Grid 
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center" >
+              <Grid item xs={5} >
+                <CarroTextField
+                  label="Numar card"
+  
+                  placeholder=""
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={5}>
+              <CarroTextField
+                id="date"
+                label="Data expirare"
+                type="date"
+                defaultValue="2017-05-24T10:30"
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+              />
+              </Grid>
+            </Grid>
+          </Box>
+          <Box my={4}>
+            <Grid 
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center" >
+              <Grid item xs={5}>
+                <CarroTextField
+                  label="Nume complet"
+  
+                  placeholder=""
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <CarroTextField
+                  label="CVV/CVC"
+                  placeholder="" 
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Box display='flex' direction='row' justifyContent='flex-end'> 
+            <Grid item xs={3}>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={stateSave.checkedA}
+                      onChange={handleChangeSave}
+                      name="checkedA"
+                      color="default"
+                    />
+                  }
+                  label="Salveaza datele"
+                />
+              </FormGroup>
+            </Grid>
+          </Box>
         </Container>
       );
     // default:
