3c1852eecb890045e20c9617dd15663bf52a89cb
Vlad Timu <vlad.timu@smartboxdigital.com>
2021-09-28 12:17:58 +0300

    refactor

--- web-platform/src/pages/register/registerStyles.jsx
+++ web-platform/src/pages/register/registerStyles.jsx
@@ -6,43 +6,7 @@ const useStyles = makeStyles({
 
     label: {fontSize: '16px',},
 
-    AccordionDetailsFlex: {
-      display: "flex",
-      flexDirection: "column",
-    },
-
-    SocialMediaAlign: {
-      display: "flex",
-      justifyCcontent: "space-evenly",
-      width: "100%",
-      alignItems: "center",
-      marginTop: "35px",
-    },
-
-    ButtonWidth: {
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center'
-    },
-
-    PrimaryButton:{
-      display: 'flex',
-      justifyContent: 'center',
-      width: '100%',
-      height: '42px',
-      backgroundColor: '#00B4D8',
-      margin: 'auto',
-      marginTop: '20px',
-      marginBottom: '20px',
-      '&:hover': {
-        backgroundColor: '#FFFFFF',
-        color: '#00B4D8',
-      },
-      TextFieldOutline: {
-        '&.Mui-focused fieldset': {borderColor: '#00B4D8'},
-                                  CheckBoxStyle:{fontSize: '16px'},
-      },
-    },
+    
 });
 
 export default useStyles;
--- web-platform/src/pages/register/select-plan/SelectPlanStyles.jsx
+++ web-platform/src/pages/register/select-plan/SelectPlanStyles.jsx
@@ -7,48 +7,33 @@ const useStyles = makeStyles({
     borderRadius: "15px",
     boxShadow: "0 4px 6px rgba(0, 0, 0, 0.31)",
   },
-  SelectPlanContainterWidth: {
-    width: "50%",
-    marginTop: '100px',
-    marginBottom: '90px',
-  },
-  SmallContainerContent: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: "20px",
-  },
-  SmallContainerContentText: {
-    fontStyle: "normal",
-    fontWeight: "500",
-    fontSize: "16px",
-    lineHeight: "150%",
-    marginBottom: '20px',
-  },
-  CheckingBox: {
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: 'center',
-    margin: "10px",
-    fontSize: "22px",
-  },
-  DeclineIcon: {
-  width: '16px',
-  height: '16px',
-  },
-  SmallContainerPackage: {
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    lineHeight: '150%',
-    letterSpacing: '0.15px',
-    color: '#00B4D8',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: '10px',
-    marginTop: '20px'
-  },
+ 
+ 
+
+  // CheckingBox: {
+  //   display: "flex",
+  //   justifyContent: "space-evenly",
+  //   alignItems: 'center',
+  //   margin: "10px",
+  //   fontSize: "22px",
+  // },
+  // DeclineIcon: {
+  // width: '16px',
+  // height: '16px',
+  // },
+  // SmallContainerPackage: {
+  //   fontStyle: 'normal',
+  //   fontWeight: 'bold',
+  //   fontSize: '16px',
+  //   lineHeight: '150%',
+  //   letterSpacing: '0.15px',
+  //   color: '#00B4D8',
+  //   display: 'flex',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   marginBottom: '10px',
+  //   marginTop: '20px'
+  // },
 });
 
 export default useStyles;
