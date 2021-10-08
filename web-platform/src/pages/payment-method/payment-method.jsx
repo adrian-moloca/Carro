4f433fb4c2fc5b8c053ae1e859811143c27c575b
Vlad Timu <vlad.timu@smartboxdigital.com>
2021-10-08 15:11:51 +0300

    payment methods

--- web-platform/src/pages/payment-method/payment-method.jsx
+++ web-platform/src/pages/payment-method/payment-method.jsx
@@ -4,11 +4,11 @@ import {CreditCard} from '@material-ui/icons/';
 import { Link } from 'react-router-dom';
 import PrimaryButton from '../../components/buttons/primaryButton/primaryButton';
 import CardSelected from '../../components/card-selected/card-selected';
-
+import { useTranslation } from "react-i18next";
 //TREBUIE FACUT REVIEW PE useLayoutEffect
 
 const PaymentMethod = () => {
-  
+  const { t } = useTranslation();
   const [cardSetted, setCardSetted] =  useState(localStorage.getItem('paymentMethodExist'));
 
   const [paymentMethod, setPaymenthMethod] = useState('no assignment');
@@ -17,7 +17,7 @@ const PaymentMethod = () => {
     <Container column className='Primary-container-style'>
       <Grid container xs={12} justifyContent='center' spacing={3}>
         <Grid container item xs={12} justifyContent='center'> 
-          <Box mb={3} fontSize={22} justifyContent='center' mt='3%'>Metoda de plata</Box>
+          <Box mb={3} fontSize={22} justifyContent='center' mt='3%'>{t("PaymentMethod")}</Box>
         </Grid>
         {cardSetted ? 
                 (
@@ -28,13 +28,13 @@ const PaymentMethod = () => {
                   <Fragment>
                     <Grid container item xs={12} justifyContent='center'>
                       <Box fontSize={22} display='flex' justifyContent='center' mt='10%' className={'Secondary-color'}>
-                          Nu aveti nici o metoda de plata adaugata!
+                         {t("NoPayment")}
                       </Box>
                     </Grid>
                       <Grid container item xs={12} justifyContent='center'>
                         <Box display='flex' justifyContent='center' mt='10%'>
                             <Link to='/payment-method/add-payment-method' style={{textDecoration: 'none'}}>
-                              <PrimaryButton variant = 'contained' size='large' endIcon={<CreditCard/>} fullWidth>Adauga metoda de plata</PrimaryButton>
+                              <PrimaryButton variant = 'contained' size='large' endIcon={<CreditCard/>} fullWidth>{t("AddPayment")}</PrimaryButton>
                             </Link>
                         </Box>
                       </Grid>
