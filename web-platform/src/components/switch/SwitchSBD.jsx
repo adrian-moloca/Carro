e09819da30d41d7049d3dc4267fc51dd35bcbed1
AlexandruSlavoaca <alexandru.slavoaca@smartboxdigital.com>
2021-10-11 15:58:07 +0300

    admin panel

--- web-platform/src/components/switch/SwitchSBD.jsx
+++ web-platform/src/components/switch/SwitchSBD.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
-import DeleteModal from '../modals/deleteModal/DeleteModal';
-import {FormControlLabel, Switch} from '@material-ui/core';
+import {FormControlLabel, Switch, Modal, Box} from '@material-ui/core';
+import SecondaryButton from '../buttons/secondaryButton/secondaryButton';
+import useStyles from '../modals/deleteModal/DeleteModalStyle'
 
 const SwitchSBD = (props) => {
 
-  const [stateCheckedPremimum, setStateCheckedPremimum] = React.useState({
-    checkedPremiumA: true,
-    checkedPremiumB: false,
-  });
-  const handleChangePremium = (event) => {
-    setStateCheckedPremimum({ ...stateCheckedPremimum, [event.target.namePremium]: event.target.checkedPremium });
-  };
+  const { content, btn1Text, btn2Text } = props;
+  const classes = useStyles();
 
-  const [stateCheckedValidate, setStateCheckedValidate] = React.useState({
-    checkedValidateA: true,
-    checkedValidateB: false,
-  });
-  const handleChangeValidate = (event) => {
-    setStateCheckedValidate({ ...stateCheckedValidate, [event.target.nameValidate]: event.target.checkedValidate });
-  };
+  // modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // switch
+  const [state, setState] = React.useState({
+    checkedA: true ,
+    checkedB: true ,
+   });
+   const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked});
+   };
 
   return (
+    <Box>
       <FormControlLabel
         control={
           <Switch
-            checkedPremium={stateCheckedPremimum.checkedPremiumB}
-            onChangePremium={handleChangePremium}
-            checkedValidate={stateCheckedValidate.checkedValidateB}
-            onChangeValidate={handleChangeValidate}
-            namePremium="checkedPremiumB"
-            nameValidate="checkedValidateB"
-            color="primary"
+            onClick={handleOpen}
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"    
+            color= "primary"
           />
         }
       />
-
+      <Modal
+        open={open}
+        className={classes.modal}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"  className={classes.paper}>
+          <Box fontSize={24} color="#3F3F3F" >{content}</Box>
+          <Box display="flex" mt={3}>
+            <Box mr={2}>
+              <SecondaryButton variant="outlined" onClick={handleClose}>{btn1Text}</SecondaryButton>
+            </Box>
+            <Box ml={2}>
+              <SecondaryButton variant="contained">{btn2Text}</SecondaryButton>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
   );
 }
 
