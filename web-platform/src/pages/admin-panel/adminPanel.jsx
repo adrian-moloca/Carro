87778f3172bd8015c2ea474a40fd244813c57004
AlexandruSlavoaca <alexandru.slavoaca@smartboxdigital.com>
2021-10-12 17:40:23 +0300

    Update adminPanel.jsx

--- web-platform/src/pages/admin-panel/adminPanel.jsx
+++ web-platform/src/pages/admin-panel/adminPanel.jsx
@@ -18,33 +18,8 @@ const [visibleLanguage, setVisibleLanguage] = useState(false);
       <Box mb={2} fontWeight={400} fontSize={25} textAlign={"center"}>
         Admin Panel
       </Box>
-      <Box mt={7}>
-        <Grid container xs={12} justifyContent="space-between">
-          <Grid container item xs={5} justifyContent="flex-end">
-          {visibleUsers ? (<PrimaryAdminButton variant="contained" onClick={() => setVisibleUsers(true)}> Users</PrimaryAdminButton>) : (<SecondaryAdminButton variant="contained" onClick={() => setVisibleUsers(!visibleUsers)}>
-              Users
-            </SecondaryAdminButton>)}
-          </Grid>
-          <Grid container item xs={5} justifyContent="flex-start">
-            {visibleUsers ? (<SecondaryAdminButton variant="contained" onClick={() => setVisibleUsers(!visibleUsers)}>
-              Language
-            </SecondaryAdminButton>) : (<PrimaryAdminButton variant="contained" onClick={() => setVisibleUsers(false)}> Language</PrimaryAdminButton>)}
-          </Grid>
-        </Grid>
-      </Box>
       <Box mt={5}>
         <Grid container xs={12} justifyContent="center">
-          <Grid container item xs={5} justifyContent="center">
-            <PrimaryButton variant="contained">
-              {" "}
-              ADAUGA UTILIZATORI
-            </PrimaryButton>
-          </Grid>
-        </Grid>
-      </Box>
-      {visibleUsers ? 
-      (<Box mt={5}>
-        <Grid container xs={12} justifyContent="flex-start">
           <Grid container item xs={4} justifyContent="flex-start">
             <CarroTextField
               required
@@ -58,7 +33,7 @@ const [visibleLanguage, setVisibleLanguage] = useState(false);
         <Box mt={5}>
         <AdminTable/>
         </Box>
-      </Box>) : 'Limba'}
+      </Box>
     </Container>
   );
 };
