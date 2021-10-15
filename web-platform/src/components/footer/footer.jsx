5180e04ac1482cd0400d44e043ebc6253c89d5af
Vlad Timu <vlad.timu@smartboxdigital.com>
2021-10-15 14:37:13 +0300

    header-footer

--- web-platform/src/components/footer/footer.jsx
+++ web-platform/src/components/footer/footer.jsx
@@ -4,24 +4,24 @@ import facebook from "../../assets/icon/facebookIcon.png"
 import instagram from "../../assets/icon/instagramIcon.png"
 import twitter from "../../assets/icon/twitterIcon.png"
 import useStyles from'./footerStyle'
-
+import { useTranslation } from 'react-i18next';
 export default function NestedGrid() {
   const classes = useStyles();
-
+  const { t } = useTranslation();
   function FormRow() {
     return (
       <Box mt={2}>
         <Grid item >
-          <Paper className={classes.paper}>Cum functioneaza</Paper>
+          <Paper className={classes.paper}>{t("HowItWorks")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Despre noi</Paper>
+          <Paper className={classes.paper}>{t("AboutUs")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Intrebari frecvente</Paper>
+          <Paper className={classes.paper}>{t("FAQ")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Noutati si planuri de viitor</Paper>
+          <Paper className={classes.paper}>{t("NewsAndFuturesPlans")}</Paper>
         </Grid>
       </Box>
     );
@@ -33,13 +33,13 @@ export default function NestedGrid() {
           <Paper className={classes.paper}>Contact</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Termeni si conditii</Paper>
+          <Paper className={classes.paper}>{t("TermsAndConditions")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Presa</Paper>
+          <Paper className={classes.paper}>{t("Press")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Recenzii</Paper>
+          <Paper className={classes.paper}>{t("Reviews")}</Paper>
         </Grid>
       </Box>
     );
@@ -48,16 +48,16 @@ export default function NestedGrid() {
     return (
       <Box mt={2}>
         <Grid item >
-          <Paper className={classes.paper}>Cariera</Paper>
+          <Paper className={classes.paper}>{t("Career")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Aplicatie pentru mobil</Paper>
+          <Paper className={classes.paper}>{t("MobileApplication")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>De ce sa folosesti serviciile noastre</Paper>
+          <Paper className={classes.paper}>{t("WhyUseOurServices")}</Paper>
         </Grid>
         <Grid item >
-          <Paper className={classes.paper}>Vouchere</Paper>
+          <Paper className={classes.paper}>{t("Vouchers")}</Paper>
         </Grid>
       </Box>
     );
