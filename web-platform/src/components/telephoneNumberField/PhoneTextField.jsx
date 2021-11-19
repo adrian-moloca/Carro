57e4625e5d5dd9fc23d13b084ef9ca6630cc824a
AdrianUrs00 <adrian.urs@smartboxdigital.com>
2021-11-19 20:59:44 +0200

    19/11/2021 2

--- web-platform/src/components/telephoneNumberField/PhoneTextField.jsx
+++ web-platform/src/components/telephoneNumberField/PhoneTextField.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Select, InputAdornment, MenuItem } from '@material-ui/core';
+import { Select, InputAdornment, MenuItem, Grid } from '@material-ui/core';
 import CarroTextField from '../textField/CarroTextField';
 import {Country} from 'country-state-city';
 import { useTranslation } from 'react-i18next';
+import phoneValidator from '../../utils/Functions/phone-validator';
 const PhoneTextField = (props) =>{
     const { t } = useTranslation();
     return(
-        <CarroTextField 
-            type='number'
-            label={t('PhoneNumber')} 
-            variant='outlined'  
-            InputProps={{startAdornment: 
-                    <InputAdornment position='start'>
-                        <Select value={props.countryPhoneCode} onChange={(event)=>props.handleSelectCountry(event)}>
-                            {Country.getAllCountries().map((country, index)=>(
-                                <MenuItem key={index} value={country.phonecode}>
-                                    {country.name} {country.phonecode.includes('+') ? '': '+'}{country.phonecode}
-                                </MenuItem> 
-                            ))}
-                        </Select>
-                    </InputAdornment>}} 
-            size={props.size}
-            fullWidth
-            {...props}/>
+        <Grid container item justifyContent='space-between'>
+            <Grid container item xs={3} alignItems='center'>
+                <CarroTextField select value={props.countryPhoneCode} onChange={props.handleSelectCountry} fullWidth>
+                    {Country.getAllCountries().map((country, index)=>(
+                        <MenuItem key={index} value={country.phonecode}>
+                            {country.name} {country.phonecode.includes('+') ? '': '+'}{country.phonecode}
+                        </MenuItem> 
+                    ))}
+                </CarroTextField>
+            </Grid>
+            <Grid container item xs={8}>
+                <CarroTextField 
+                    type='number'
+                    label={t('PhoneNumber')} 
+                    variant='outlined'  
+                    fullWidth
+                    value={props.value}
+                    onChange={props.onChange}/>
+            </Grid>
+        </Grid>
     );
 
 }
