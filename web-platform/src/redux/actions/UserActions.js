7d5e67bc6667968e65b41f8a6a6b0cbf30c5d6e2
AdrianUrs00 <adrian.urs00@gmail.com>
2022-05-25 23:48:26 +0300

    fixed google login

--- web-platform/src/redux/actions/UserActions.js
+++ web-platform/src/redux/actions/UserActions.js
@@ -1,11 +1,12 @@
-import {fetchLoginRequest, fetchLoginSuccess, fetchLoginFailure,
+import {
+    fetchLoginRequest, fetchLoginSuccess, fetchLoginFailure,
     fetchLogout, createNewUserRequest, createNewUserSuccess, createNewUserFailure,
     fetchUsersRequest, fetchUsersSuccess, fetchUsersFailure,
     updateUserRequest, updateUserSuccess, updateUserFailure,
     deleteUserRequest, deleteUserSuccess, deleteUserFailure,
     getUserProfileImageRequest, getUserProfileImageSuccess, getUserProfileImageFailure,
     getUserPersonalInfoRequest, getUserPersonalInfoSuccess, getUserPersonalInfoFailure,
-    getUserOptionalInfoRequest, getUserOptionalInfoSuccess, getUserOptionalInfoFailure, 
+    getUserOptionalInfoRequest, getUserOptionalInfoSuccess, getUserOptionalInfoFailure,
     getUserCompanyRequest, getUserCompanySuccess, getUserCompanyFailure,
     getProfileStatusRequest, getProfileStatusSuccess, getProfileStatusFailure,
     changePasswordUserRequest, changePasswordUserSuccess, changePasswordUserFailure,
@@ -18,118 +19,118 @@ import jwt_decode from "jwt-decode";
 
 export const fetchLogin = (email, password) => {
 
-return (dispatch) => {
-    dispatch(fetchLoginRequest());
-    axios.post(data.baseUrl + "/identity/login", {
-        email: email, 
-        password: password
-    })
-    .then(response => {
-        getProfileStatus(response.data.token)
-        dispatch(fetchLoginSuccess(jwt_decode(response.data.token), response.data.token, response.data.refreshToken));
-    }).catch(error => {
-        const errorMsg = error;
-        dispatch(fetchLoginFailure(errorMsg));
-    })
-}
+    return (dispatch) => {
+        dispatch(fetchLoginRequest());
+        axios.post(data.baseUrl + "/identity/login", {
+            email: email,
+            password: password
+        })
+            .then(response => {
+                getProfileStatus(response.data.token)
+                dispatch(fetchLoginSuccess(jwt_decode(response.data.token), response.data.token, response.data.refreshToken));
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(fetchLoginFailure(errorMsg));
+            })
+    }
 }
 
 
-export async function fetchUsers () {
+export async function fetchUsers() {
 
     return (dispatch) => {
         dispatch(fetchUsersRequest());
         axios.get(data.baseUrl + "/catalin/admin/users/")
-        .then(response => {
-            const user = response.data;
-            dispatch(fetchUsersSuccess(user));
-            return true
-        }).catch(error => {
-            const errorMsg = error;
-            dispatch(fetchUsersFailure(errorMsg))
-            return false
-        })
+            .then(response => {
+                const user = response.data;
+                dispatch(fetchUsersSuccess(user));
+                return true
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(fetchUsersFailure(errorMsg))
+                return false
+            })
     }
 }
 
 
 export const createNewUser = (email, password, phoneNumber, firstName, lastName, dateOfBirth, termsAndConditions) => {
 
-return(dispatch) => {
-dispatch(createNewUserRequest());
-axios.post(data.baseUrl+"/identity/register",{
-    email,
-    password,
-    phoneNumber,
-    firstName,
-    lastName,
-    dateOfBirth,
-    termsAndConditions
+    return (dispatch) => {
+        dispatch(createNewUserRequest());
+        axios.post(data.baseUrl + "/identity/register", {
+            email,
+            password,
+            phoneNumber,
+            firstName,
+            lastName,
+            dateOfBirth,
+            termsAndConditions
 
-})
-.then(response => {
-    dispatch(createNewUserSuccess(jwt_decode(response.data.token), response.data.token, response.data.refreshToken));
-}).catch(error => {
-    const errorMsg = error;
-    dispatch(createNewUserFailure(errorMsg))
-})
-}
+        })
+            .then(response => {
+                dispatch(createNewUserSuccess(jwt_decode(response.data.token), response.data.token, response.data.refreshToken));
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(createNewUserFailure(errorMsg))
+            })
+    }
 }
 
 export const changePasswordUser = (newPassword, confirmNewPassword, id) => {
 
-return(dispatch) => {
-    dispatch(changePasswordUserRequest());
-    axios.patch(data.baseUrl + "/catalin/admin/users/recoverPass/" + id,{
-        newPassword                 : newPassword,
-        confirm_newPassword         : confirmNewPassword,
-        
-})
-.then(response => {
-    dispatch( changePasswordUserSuccess(id));
-    dispatch(fetchUsers())
-}).catch(error => {
-    const errorMsg = error;
-    dispatch( changePasswordUserFailure(errorMsg))
-})
-}
+    return (dispatch) => {
+        dispatch(changePasswordUserRequest());
+        axios.patch(data.baseUrl + "/catalin/admin/users/recoverPass/" + id, {
+            newPassword: newPassword,
+            confirm_newPassword: confirmNewPassword,
+
+        })
+            .then(response => {
+                dispatch(changePasswordUserSuccess(id));
+                dispatch(fetchUsers())
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(changePasswordUserFailure(errorMsg))
+            })
+    }
 }
 
-export const updateUser = (newName, id, newCarNumber ) => {
+export const updateUser = (newName, id, newCarNumber) => {
 
-return(dispatch) => {
-    dispatch(updateUserRequest());
-    axios.patch(data.baseUrl+"/catalin/admin/users/" + id,{
-        name     : newName,
-        carNumber: newCarNumber,
-        
-})
-.then(response => {
-    dispatch(updateUserSuccess(id));
-    dispatch(fetchUsers())
-}).catch(error => {
-    const errorMsg = error;
-    dispatch(updateUserFailure(errorMsg))
-})
-}
+    return (dispatch) => {
+        dispatch(updateUserRequest());
+        axios.patch(data.baseUrl + "/catalin/admin/users/" + id, {
+            name: newName,
+            carNumber: newCarNumber,
+
+        })
+            .then(response => {
+                dispatch(updateUserSuccess(id));
+                dispatch(fetchUsers())
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(updateUserFailure(errorMsg))
+            })
+    }
 }
 
 export const deleteUser = (id) => {
 
-return(dispatch) => {
-dispatch(deleteUserRequest());
-axios.delete(data.baseUrl+"/catalin/admin/users/" +id ,{
-    
-})
-.then(response => {
-    const Msg = response.data;
-    dispatch(deleteUserSuccess(Msg));
-    dispatch(fetchUsers())
-}).catch(error => {
-    const errorMsg = error;
-    dispatch(deleteUserFailure(errorMsg))
-})
-}
+    return (dispatch) => {
+        dispatch(deleteUserRequest());
+        axios.delete(data.baseUrl + "/catalin/admin/users/" + id, {
+
+        })
+            .then(response => {
+                const Msg = response.data;
+                dispatch(deleteUserSuccess(Msg));
+                dispatch(fetchUsers())
+            }).catch(error => {
+                const errorMsg = error;
+                dispatch(deleteUserFailure(errorMsg))
+            })
+    }
 }
 
 export const getUserProfileImage = (token) => {
@@ -137,9 +138,10 @@ export const getUserProfileImage = (token) => {
     return (dispatch) => {
         dispatch(getUserProfileImageRequest());
         axios.get(data.baseUrl + "/users/profile-images", {
-            headers:{
-              'Authorization': `Bearer ${token}`,
-            }}).then(response => {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then(response => {
             const image = response.data.data;
             dispatch(getUserProfileImageSuccess(image));
         }).catch(error => {
@@ -154,9 +156,10 @@ export const getUserPersonalInfo = (token) => {
     return (dispatch) => {
         dispatch(getUserPersonalInfoRequest());
         axios.get(data.baseUrl + "/users/personal-infos", {
-            headers:{
-              'Authorization': `Bearer ${token}`,
-            }}).then(response => {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then(response => {
             const info = response.data.data;
             dispatch(getUserPersonalInfoSuccess(info));
         }).catch(error => {
@@ -171,9 +174,10 @@ export const getUserOptionalInfo = (token) => {
     return (dispatch) => {
         dispatch(getUserOptionalInfoRequest());
         axios.get(data.baseUrl + "/users/optional-infos", {
-            headers:{
-              'Authorization': `Bearer ${token}`,
-            }}).then(response => {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then(response => {
             const info = response.data.data;
             dispatch(getUserOptionalInfoSuccess(info));
         }).catch(error => {
@@ -188,9 +192,10 @@ export const getUserCompany = (token) => {
     return (dispatch) => {
         dispatch(getUserCompanyRequest());
         axios.get(data.baseUrl + "/users/companies", {
-            headers:{
-              'Authorization': `Bearer ${token}`,
-            }}).then(response => {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then(response => {
             const company = response.data.data;
             dispatch(getUserCompanySuccess(company));
         }).catch(error => {
@@ -205,9 +210,10 @@ export const getProfileStatus = (token) => {
     return (dispatch) => {
         dispatch(getProfileStatusRequest());
         axios.get(data.baseUrl + "/users/user-statuses", {
-            headers:{
-              'Authorization': `Bearer ${token}`,
-            }}).then(response => {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then(response => {
             const statuses = response.data.data;
             dispatch(getProfileStatusSuccess(statuses));
         }).catch(error => {
@@ -219,12 +225,23 @@ export const getProfileStatus = (token) => {
 
 export const googleLogin = (token) => {
     return (dispatch) => {
-        dispatch(googleLoginSucces({...jwt_decode(token), token: token}))
+        dispatch(fetchLoginRequest());
+        axios.post(data.baseUrl + "/identity/auth/google", {
+            accessToken: token
+        })
+        .then(response => {
+            console.log(jwt_decode(response.data.token))
+            getProfileStatus(response.data.token)
+            dispatch(googleLoginSucces({...jwt_decode(response.data.token),token: response.data.token,refreshToken: response.data.refreshToken}));
+        }).catch(error => {
+            const errorMsg = error;
+            dispatch(fetchLoginFailure(errorMsg));
+        })
     }
 }
 
 export const refreshToken = (token, refreshToken) => {
     return (dispatch) => {
-        dispatch(refreshTokens({token: token, refreshToken: refreshToken}))   
+        dispatch(refreshTokens({ token: token, refreshToken: refreshToken }))
     }
 }
